f984b3f138d5b16a1e7ab0cde2b37f575dfaac55
LimJongJin <dlawhdwls00@naver.com>
2015-11-02 21:13:01 +0900

    PVR 녹화예약관리, 녹화물목록 데이터 받아오나 갱신이 안됩니다.

--- NScreen/app/src/main/java/com/stvn/nscreen/pvr/PvrMainActivity.java
+++ NScreen/app/src/main/java/com/stvn/nscreen/pvr/PvrMainActivity.java
@@ -7,8 +7,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
@@ -48,7 +50,10 @@ public class PvrMainActivity extends AppCompatActivity {
 
     private              PvrMainListViewAdapter mAdapter;
     private              ListView               mListView;
-    private ImageButton pvr_main_backBtn;
+
+    private              ImageButton            pvr_main_backBtn;
+    private              Button                 button1, button2;
+    private              TextView               textView2, textView7;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -70,17 +75,39 @@ public class PvrMainActivity extends AppCompatActivity {
         if (mPref.isLogging()) { Log.d(tag, "onCreate()"); }
 
         mAdapter  = new PvrMainListViewAdapter(this, null);
-        // for test
-//        for ( int i = 0; i < 1000; i++ ) {
-//            String sChannel        = String.format("%02d", i);
-//            ListViewDataObject obj = new ListViewDataObject(0, 0, "{\"channelNumber\":\""+sChannel+"\",\"title\":\"전국 노래자랑 광진구편 초대가수 임석원 사회 송해\"}");
-//            mAdapter.addItem(obj);
-//        }
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+
+        textView2 = (TextView) findViewById(R.id.textView2);
+        textView7 = (TextView) findViewById(R.id.textView7);
 
         mListView = (ListView)findViewById(R.id.pvr_main_listview);
         mListView.setAdapter(mAdapter);
 
-        requestGetRecordReservelist();
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button1.setSelected(true);
+                button2.setSelected(false);
+                textView2.setVisibility(View.VISIBLE);
+                textView7.setVisibility(View.GONE);
+                requestGetRecordReservelist();
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button1.setSelected(false);
+                button2.setSelected(true);
+                textView2.setVisibility(View.GONE);
+                textView7.setVisibility(View.VISIBLE);
+                requestGetrecordlist();
+            }
+        }); {
+
+        }
     }
 
     private void requestGetRecordReservelist() {
@@ -137,6 +164,76 @@ public class PvrMainActivity extends AppCompatActivity {
                         sb.append(",\"ProgramName\":\"").append(xpp.nextText()).append("\"");
                     } else if (xpp.getName().equalsIgnoreCase("RecordStatus")) {
                         sb.append(",\"RecordStatus\":\"").append(xpp.nextText()).append("\"}");
+                        ListViewDataObject obj = new ListViewDataObject(mAdapter.getCount(), 0, sb.toString());
+                        mAdapter.addItem(obj);
+                        sb.setLength(0);
+                    }
+                }
+                eventType = xpp.next();
+            }
+        } catch (XmlPullParserException e) {
+            e.printStackTrace();
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void requestGetrecordlist() {
+        mProgressDialog	 = ProgressDialog.show(mInstance,"",getString(R.string.wait_a_moment));
+        if ( mPref.isLogging() ) { Log.d(tag, "requestGetrecordlist()"); }
+        String uuid = mPref.getValue(JYSharedPreferences.UUID, "");
+        String tk   = mPref.getWebhasTerminalKey();
+        String url  = mPref.getRumpersServerUrl() + "/getrecordlist.asp?Version=1&terminalKey=" + tk + "&deviceId=" + uuid;
+        JYStringRequest request = new JYStringRequest(mPref, Request.Method.GET, url, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Log.d(tag, response);
+                mProgressDialog.dismiss();
+                parseGetrecordlist(response);
+                mAdapter.notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                mProgressDialog.dismiss();
+                if ( mPref.isLogging() ) { VolleyLog.d(tag, "onErrorResponse(): " + error.getMessage()); }
+            }
+        }) {
+            @Override
+            protected Map<String,String> getParams(){
+                Map<String,String> params = new HashMap<String, String>();
+                params.put("version", String.valueOf(1));
+                params.put("areaCode", String.valueOf(0));
+                if ( mPref.isLogging() ) { Log.d(tag, "getParams()" + params.toString()); }
+                return params;
+            }
+        };
+        mRequestQueue.add(request);
+    }
+
+    private void parseGetrecordlist(String response) {
+        StringBuilder sb = new StringBuilder();
+        XmlPullParserFactory factory = null;
+        try {
+            factory = XmlPullParserFactory.newInstance();
+            factory.setNamespaceAware(true);
+
+            XmlPullParser xpp = factory.newPullParser();
+            xpp.setInput(new ByteArrayInputStream(response.getBytes("utf-8")), "utf-8");
+
+            int eventType = xpp.getEventType();
+            while ( eventType != XmlPullParser.END_DOCUMENT ) {
+                if (eventType == XmlPullParser.START_TAG) {
+                    if (xpp.getName().equalsIgnoreCase("RecordId")) {
+                        sb.append("{\"RecordId\":\"").append(xpp.nextText()).append("\"");
+                    } else if (xpp.getName().equalsIgnoreCase("Channel_logo_img")) {
+                        sb.append(",\"Channel_logo_img\":\"").append(xpp.nextText()).append("\"");
+                    } else if (xpp.getName().equalsIgnoreCase("ProgramName")) {
+                        sb.append(",\"ProgramName\":\"").append(xpp.nextText()).append("\"");
+                    } else if (xpp.getName().equalsIgnoreCase("RecordHD")) {
+                        sb.append(",\"RecordHD\":\"").append(xpp.nextText()).append("\"}");
                         ListViewDataObject obj = new ListViewDataObject(mAdapter.getCount(), 0, sb.toString());
                         mAdapter.addItem(obj);
                         sb.setLength(0);
