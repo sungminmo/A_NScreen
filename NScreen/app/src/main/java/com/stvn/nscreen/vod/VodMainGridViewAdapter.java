b742b2bc40477a9350fc2aa9e60e45bd770c35c1
LimJongJin <dlawhdwls00@naver.com>
2015-11-12 06:44:20 +0900

    VOD 카테고리 별 포스터 내의 스티커 표시.

--- NScreen/app/src/main/java/com/stvn/nscreen/vod/VodMainGridViewAdapter.java
+++ NScreen/app/src/main/java/com/stvn/nscreen/vod/VodMainGridViewAdapter.java
@@ -1,10 +1,12 @@
 package com.stvn.nscreen.vod;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.android.volley.toolbox.ImageLoader;
@@ -18,6 +20,8 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+
+import static com.jjiya.android.common.UiUtil.setPromotionSticker;
 
 /**
  * Created by swlim on 2015. 9. 11..
@@ -67,10 +71,57 @@ public class VodMainGridViewAdapter extends BaseAdapter {
 
             NetworkImageView vodImageView = ViewHolder.get(convertView, R.id.volleyImageView);
             TextView titleTextView        = ViewHolder.get(convertView, R.id.vod_main_textview_title);
-            TextView rankTextView         = ViewHolder.get(convertView, R.id.vod_main_grid_rank_textview);
+            TextView rankTextView         = ViewHolder.get(convertView, R.id.gridview_vod_main_ranking_textview);
+            TextView gridview_vod_main_ranking_textview = ViewHolder.get(convertView, R.id.gridview_vod_main_ranking_textview);
+            ImageView gridview_vod_main_ranking_imageview = ViewHolder.get(convertView, R.id.gridview_vod_main_ranking_imageview);
+            ImageView gridview_vod_main_promotionsticker_imageview = ViewHolder.get(convertView, R.id.gridview_vod_main_promotionsticker_imageview);
+            ImageView gridview_vod_main_19_imageview = ViewHolder.get(convertView, R.id.gridview_vod_main_19_imageview);
+            ImageView gridview_vod_main_tvonly_imageview = ViewHolder.get(convertView, R.id.gridview_vod_main_tvonly_imageview);
+
+            if ( jo.isNull("ranking") ) {
+                // 랭킹 없음.
+                gridview_vod_main_ranking_imageview.setVisibility(View.GONE);
+                gridview_vod_main_ranking_textview.setVisibility(View.GONE);
+            } else {
+                // 랭킹 있음.
+                rankTextView.setText(jo.getString("ranking"));
+            }
+
+            String publicationRight = null;
+            if ( ! jo.isNull("publicationRight") ) {
+                publicationRight = jo.getString("publicationRight");
+            } else if ( ! jo.isNull("mobilePublicationRight") ) {
+                publicationRight = jo.getString("mobilePublicationRight");
+            }
+            String publicationRight1 = publicationRight;
 
             titleTextView.setText(jo.getString("title"));
-            rankTextView.setText(jo.getString("ranking"));
+            String promotionSticker = jo.getString("promotionSticker");
+            String rating = jo.getString("rating");
+
+            setPromotionSticker(promotionSticker, gridview_vod_main_promotionsticker_imageview);
+            if ( "1".equals(publicationRight1) ) {
+                gridview_vod_main_tvonly_imageview.setVisibility(View.VISIBLE);
+            }
+            if ( "19".equals(rating) ) {
+                gridview_vod_main_19_imageview.setVisibility(View.VISIBLE);
+            }
+            String assetId = null;
+            if ( ! jo.isNull("assetId") ) {
+                assetId = jo.getString("assetId");
+            } else if ( ! jo.isNull("primaryAssetId") ) {
+                assetId = jo.getString("primaryAssetId");
+            }
+            final String assetId1 = assetId;
+            vodImageView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(mContext, VodDetailActivity.class);
+                    intent.putExtra("assetId", assetId1);
+                    mContext.startActivity(intent);
+                }
+            });
+
 
             String imageFileName = jo.getString("imageFileName");
             vodImageView.setImageUrl(imageFileName, mImageLoader);
