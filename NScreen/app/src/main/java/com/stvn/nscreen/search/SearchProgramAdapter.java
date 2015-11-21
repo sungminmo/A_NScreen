356ac698763e49395c5bf772f55759fb0532b3df
lastaosi <lastaosi@gmail.com>
2015-11-21 17:56:47 +0900

    프로그램검색 관련 오류수정 및 레이어팝업 기능 연동

--- NScreen/app/src/main/java/com/stvn/nscreen/search/SearchProgramAdapter.java
+++ NScreen/app/src/main/java/com/stvn/nscreen/search/SearchProgramAdapter.java
@@ -10,7 +10,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.ProgressBar;
 import android.widget.TextView;
 
 import com.android.volley.RequestQueue;
@@ -23,6 +22,12 @@ import com.stvn.nscreen.common.SearchProgramDataObject;
 import com.stvn.nscreen.common.SwipeListView;
 import com.stvn.nscreen.util.CMUtil;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> {
@@ -39,6 +44,13 @@ public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> 
     private String mStbRecordingchannel2; // GetSetTopStatus API로 가져오는 값.
     private String mStbWatchingchannel;   // GetSetTopStatus API로 가져오는 값.
     private String mStbPipchannel;        // GetSetTopStatus API로 가져오는 값.
+    private ArrayList<JSONObject> mStbRecordReservelist;
+    private              int                           mCurrDateNo;
+    private SimpleDateFormat mTodayFormat;
+    private SimpleDateFormat mCompareFormat;
+    private Date mToday;
+    private ViewGroup mParent;
+
 
     public SearchProgramAdapter(Context context, List<SearchProgramDataObject> items) {
         super(context, 0, items);
@@ -47,6 +59,9 @@ public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> 
         mInflater = LayoutInflater.from(context);
         mPref = new JYSharedPreferences(mContext);
         mRequestQueue = Volley.newRequestQueue(mContext);
+        mTodayFormat = new SimpleDateFormat("yyyyMMdd");
+        mCompareFormat = new SimpleDateFormat("yyyy-MM-ddHH:mm:ss");
+        mToday = new Date();
         mImageLoader = new ImageLoader(mRequestQueue, new ImageLoader.ImageCache() {
             private final LruCache<String, Bitmap> mCache = new LruCache<String, Bitmap>(100);
 
@@ -90,7 +105,6 @@ public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> 
             holder.channeltext2 = (TextView) convertView.findViewById(R.id.ch_text2);
             holder.chage = (ImageView) convertView.findViewById(R.id.ch_age);
             holder.chhd = (ImageView) convertView.findViewById(R.id.ch_hd);
-            holder.progress = (ProgressBar) convertView.findViewById(R.id.ch_progress);
             holder.notPairing = (Button) convertView.findViewById(R.id.not_pairing);
             holder.recstart = (Button) convertView.findViewById(R.id.rec_start);
             holder.recstop = (Button) convertView.findViewById(R.id.rec_stop);
@@ -112,6 +126,7 @@ public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> 
             holder = (ViewHolder) convertView.getTag();
         }
 
+        mParent = parent;
         ((SwipeListView) parent).recycle(convertView, position);
         SearchProgramDataObject item = getItem(position);
         holder.chlogo.setImageUrl(item.getChannelLogoImg(), mImageLoader);
@@ -130,6 +145,11 @@ public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> 
         } else                                                    // 둘다 아닐때
             holder.chhd.setVisibility(View.GONE);
 
+        if(mPref.isBookmarkChannelWithChannelId(item.getChannelId()))
+            holder.favoriteimg.setSelected(true);
+        else
+            holder.favoriteimg.setSelected(false);
+
         // 연령제한
         if (!TextUtils.isEmpty(item.getChannelProgramGrade())) {
             if (item.getChannelProgramGrade().indexOf("12") > -1) {
@@ -142,14 +162,16 @@ public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> 
                 holder.chage.setImageResource(R.mipmap.btn_age_all);
             }
         }
+        getSwipeLayout(item, position,holder);
+//        if (mPref.isPairingCompleted()) {
+//            getSwipeLayout(null, position, holder);
+//        }
 
-        if (mPref.isPairingCompleted()) {
-            getSwipeLayout(null, position, holder);
-        }
 
 
 
-        holder.progress.setProgress(position);
+
+
 
         return convertView;
     }
@@ -170,10 +192,134 @@ public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> 
         holder.cancelreservationrec.setTag(position);
         holder.setreservationwatch.setTag(position);
         holder.cancelreservationwatch.setTag(position);
-
+        ((SwipeListView) mParent).setSwipeMode(SwipeListView.SWIPE_MODE_DEFAULT);
         if (item == null) {
             holder.notPairing.setVisibility(View.VISIBLE);
         }
+        if(!mPref.isPairingCompleted())
+        {
+            holder.notPairing.setVisibility(View.VISIBLE);
+        }else
+        {
+            if(!mTodayFormat.format(mToday).equals(CMUtil.getConverDateString(item.getChannelProgramTime().trim(), "yyyy-MM-ddHH:mm:ss", "yyyyMMdd")))// 오늘이 아니라면...
+            {
+                JSONObject reservItem = getStbRecordReserveWithChunnelId(item.getChannelId(),item);
+                if(reservItem == null)  // 예약녹화 걸려있지 않은 방송
+                {
+                    if ( mPref.isWatchTvReserveWithProgramId(item.getChannelProgramID()) == true ) // 시청예약 걸려있음
+                    {
+                        holder.setreservationrec.setVisibility(View.VISIBLE);               // 녹화예약 시작
+                        holder.cancelreservationwatch.setVisibility(View.VISIBLE);          // 시청예약 취소
+                    }else // 시청예약 없음
+                    {
+                        holder.setreservationrec.setVisibility(View.VISIBLE);           // 녹화예약 시작
+                        holder.setreservationwatch.setVisibility(View.VISIBLE);         // 시청예약 시작
+                    }
+                }else // 예약녹화 걸린방송
+                {
+                    if ( mPref.isWatchTvReserveWithProgramId(item.getChannelProgramID()) == true ) { // 시청예약 걸려 있음
+                        holder.cancelreservationrec.setVisibility(View.VISIBLE);            // 녹화예약 취소
+                        holder.cancelreservationwatch.setVisibility(View.VISIBLE);          // 시청예약 취소
+                    } else {  // 시청예약 없음.
+                        holder.cancelreservationrec.setVisibility(View.VISIBLE);            // 녹화예약 취소
+                        holder.setreservationwatch.setVisibility(View.VISIBLE);         // 시청예약 시작
+                    }
+                }
+            }else       // 오늘일경우
+            {
+                try
+                {
+                    Date date = mCompareFormat.parse(item.getChannelProgramTime());
+                    if(mToday.compareTo(date)>0) //현재방송중이거나 과거방송
+                    {
+                        // 정책변경으로 인해 현재 방송중인 프로그램은 레이어 노출 불가
+                        ((SwipeListView) mParent).setSwipeMode(SwipeListView.SWIPE_MODE_NONE);
+                    }else
+                    {
+                        JSONObject reservItem = getStbRecordReserveWithChunnelId(item.getChannelId(), item);
+                        if (reservItem == null) { // 예약녹화 걸려있지 않은 방송.
+                            holder.setreservationrec.setVisibility(View.VISIBLE);           // 녹화예약
+                            holder.setreservationwatch.setVisibility(View.VISIBLE);         // 시청예약
+                        } else {    //  예약 녹화 걸린 방송.
+                            holder.setreservationwatch.setVisibility(View.VISIBLE);         // 시청예약
+                            holder.cancelreservationrec.setVisibility(View.VISIBLE);            // 녹화예약 취소
+                        }
+                    }
+
+                }catch(Exception e)
+                {}
+            }
+
+        }
+
+
+//        if ( mPref.isPairingCompleted() == false ) {
+//            return 6;
+//        }
+//
+//        if ( mCurrDateNo != 0 ) { // 오늘이 아니라면...
+//            JSONObject reservItem = getStbRecordReserveWithChunnelId(mChannelId, dobj);
+//            if (reservItem == null) { // 예약녹화 걸려있지 않은 방송.
+//                if ( mPref.isWatchTvReserveWithProgramId(programId) == true ) { // 시청예약 걸려 있음.
+//                    return 4; // 시청예약취소 / 녹화예약
+//                } else {  // 시청예약 없음.
+//                    return 2; // 시청예약 / 녹화예약
+//                }
+//            } else {      //  예약 녹화 걸린 방송.
+//                if ( mPref.isWatchTvReserveWithProgramId(programId) == true ) { // 시청예약 걸려 있음.
+//                    return 5; // 시청예약취소 / 녹화예약취소
+//                } else {  // 시청예약 없음.
+//                    return 3; // 시청예약 / 녹화예약취소
+//                }
+//            }
+//        } else {
+//            if ( i2 < i1) {
+//                i2 += 1440;
+//                if ( i3 < i1 ) {
+//                    i3 += 1440;
+//                    if (i1 < i3 && i3 <= i2) {
+//                        // 현재 방송 중.
+//                        // menu: TV로 시청 / 즉시녹화 or 즉시녹화중지.
+//                        if (mChannelId.equals(mStbRecordingchannel1) || mChannelId.equals(mStbRecordingchannel2)) {
+//                            return 1; // TV로 시청 / 녹화중지
+//                        } else {
+//                            return 0; // TV로 시청 / 즉시녹화
+//                        }
+//                    } else if (i3 <= i1) {
+//                        // 미래 방송.
+//                        JSONObject reservItem = getStbRecordReserveWithChunnelId(mChannelId, dobj);
+//                        if (reservItem == null) { // 예약녹화 걸려있지 않은 방송.
+//                            return 2; // 시청예약 / 녹화예약
+//                        } else {    //  예약 녹화 걸린 방송.
+//                            return 3; // 시청예약 / 녹화예약취소
+//                        }
+//                    }
+//                }
+//            } else {
+//                if (i1 < i3 && i3 <= i2) {
+//                    // 현재 방송 중.
+//                    // menu: TV로 시청은 고정./ 즉시녹화or즉시녹화중지.
+//                    if (mChannelId.equals(mStbRecordingchannel1) || mChannelId.equals(mStbRecordingchannel2)) {
+//                        return 1; // TV로 시청 / 녹화중지
+//                    } else {
+//                        return 0; // TV로 시청 / 즉시녹화
+//                    }
+//                } else if ( i3 <= i1 ) {
+//                    // 미래.
+//                    JSONObject reservItem = getStbRecordReserveWithChunnelId(mChannelId, dobj);
+//                    if (reservItem == null) { // 예약녹화 걸려있지 않은 방송.
+//                        return 2; // 시청예약 / 녹화예약
+//                    } else {    //  예약 녹화 걸린 방송.
+//                        return 3; // 시청예약 / 녹화예약취소
+//                    }
+//                } else if ( i3 > i2 ) {
+//                    // 과거.
+//
+//                }
+//            }
+//        }
+
+
 
 
 //        switch (position % 9) {
@@ -211,7 +357,11 @@ public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> 
 //                holder.cancelreservationwatch.setVisibility(View.VISIBLE);
 //                break;
 //        }
+    }
 
+
+    public void setStbRecordReservelist(ArrayList<JSONObject> list) {
+        this.mStbRecordReservelist = list;
     }
 
     class ViewHolder {
@@ -223,7 +373,6 @@ public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> 
         TextView channeltext2;
         ImageView chage;
         ImageView chhd;
-        ProgressBar progress;
 
         //Swipe 영역 버튼
         Button notPairing;//셋탑미연동
@@ -235,6 +384,33 @@ public class SearchProgramAdapter extends ArrayAdapter<SearchProgramDataObject> 
         Button setreservationwatch; // 시청예약설정
         Button cancelreservationwatch;// 시청예약취소
 
+    }
+
+    public JSONObject getStbRecordReserveWithChunnelId(String channelId, SearchProgramDataObject item) {
+        try {
+//            String str = epgitem.sJson;
+//            JSONObject epgjo = new JSONObject(str);
+//            String programBroadcastingStartTime = epgjo.getString("programBroadcastingStartTime");
+//            for ( int i = 0; i < mStbRecordReservelist.size(); i++ ) {
+//                JSONObject reservjo = mStbRecordReservelist.get(i);
+//                String RecordStartTime = reservjo.getString("RecordStartTime");
+//                if ( programBroadcastingStartTime.equals(RecordStartTime) ) {  // epg의 시작간과 예약의 시작 시간이 같다면 동일 채널 동일 프로그램.
+//                    return reservjo;
+//                }
+//            }
+            String str = item.getChannelProgramTime();
+            for(int i=0;i<mStbRecordReservelist.size();i++)
+            {
+                JSONObject reservjo = mStbRecordReservelist.get(i);
+                String RecordStartTime = reservjo.getString("RecordStartTime");
+                if (str.equals(RecordStartTime) ) {  // epg의 시작간과 예약의 시작 시간이 같다면 동일 채널 동일 프로그램.
+                    return reservjo;
+                }
+            }
+        } catch ( JSONException e ) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
 }
