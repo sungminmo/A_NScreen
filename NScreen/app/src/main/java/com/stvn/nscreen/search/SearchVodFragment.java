671e635e832f74668d056dc35349196b779f307f
Kimwoodam <kwd0808@gmail.com>
2015-11-24 18:14:37 +0900

    VOD 상세 이동시 episodePeerExistence 값에 따른 상세화면 데이터 전송 적용

--- NScreen/app/src/main/java/com/stvn/nscreen/search/SearchVodFragment.java
+++ NScreen/app/src/main/java/com/stvn/nscreen/search/SearchVodFragment.java
@@ -144,13 +144,20 @@ public class SearchVodFragment extends SearchBaseFragment implements AdapterView
     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
         String assetId = mProgramlist.get(position).primaryAssetId;
-        if(!TextUtils.isEmpty(assetId))
-        {
+        String episodePeerExistence = mProgramlist.get(position).episodePeerExistence;
+        String contentGroupId = mProgramlist.get(position).contentGroupId;
+
+        if(TextUtils.isEmpty(assetId) == false) {
             Intent intent = new Intent(getActivity(), VodDetailActivity.class);
             intent.putExtra("assetId", assetId);
+            // episodePeerExistence 값이 있으면서 1인 경우에는 아래 3가지 데이터를 추가로 VOD 상세 화면에 전달 한다.
+            if (TextUtils.isEmpty(episodePeerExistence) == false || "1".equalsIgnoreCase(episodePeerExistence) == true) {
+                intent.putExtra("episodePeerExistence", episodePeerExistence);
+                intent.putExtra("contentGroupId", contentGroupId);
+                intent.putExtra("primaryAssetId", assetId);
+            }
             startActivity(intent);
         }
-
     }
 
 }
